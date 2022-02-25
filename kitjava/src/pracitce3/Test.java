bd38b425c5010a8c6827874d391f7260f8e5334b
ywj9811 <ywj9811@naver.com>
2022-02-26 04:49:44 +0900

    pracitce

---
+++ kitjava/src/pracitce3/Test.java
@@ -0,0 +1,52 @@
+package practice3;
+
+class TextMessage{
+	private String msg;
+	
+	TextMessage(String msg) {
+		this.msg = msg;
+	}
+	
+	String getMsg() {
+		return msg;
+	}
+	
+	void show() {
+		System.out.println(msg);
+	}
+}
+
+class TextMessageVer2 extends TextMessage {
+	private String name;
+	
+	TextMessageVer2(String name, String msg) {
+		super(msg);
+		this.name = name;
+	}
+	
+	@Override
+	void show() {
+		System.out.println(name + "는 \"" + super.getMsg() + "\"라고 말씀하셨다.");
+	}
+}
+
+public class Test {
+	public static void main(String[] args) {
+		TextMessage msgObj;
+//		msgObj = new TextMessage("클래스는 공부할 게 많다.");
+//		msgObj.show();
+//		
+//		msgObj = new TextMessageVer2("YG", "반복만이 살길이다.");
+//		msgObj.show();
+		for(int i = 0; i < 2; i++) {
+			if(i == 0) {
+				msgObj = new TextMessage("클래스는 공부할 게 많다.");
+				msgObj.show();
+			}
+			else {
+				msgObj = new TextMessageVer2("YG", "반복만이 살길이다.");
+				msgObj.show();
+			}
+		}
+	}
+}
