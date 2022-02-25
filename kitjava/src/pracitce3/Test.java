ca67f9bf45160467fa07c2eb579b8e14eab43aec
ywj9811 <ywj9811@naver.com>
2022-02-26 05:35:33 +0900

    pracitce

--- kitjava/src/pracitce3/Test.java
+++ kitjava/src/pracitce3/Test.java
@@ -1,4 +1,4 @@
-package practice3;
+package pracitce3;
 
 class TextMessage{
 	private String msg;
@@ -31,13 +31,28 @@ class TextMessageVer2 extends TextMessage {
 }
 
 public class Test {
+	public static void showTextMessageAll(TextMessage[] arr) {
+		for(int i = 0; i < 2; i++) {
+			if(i == 0) {
+				arr[i] = new TextMessage("클래스는 공부할 게 많다.");
+				arr[i].show();
+				}	
+			else {
+				arr[i] = new TextMessageVer2("YG", "반복만이 살길이다.");
+				arr[i].show();
+				}
+			}
+		}
 	public static void main(String[] args) {
-		TextMessage msgObj;
-		msgObj = new TextMessage("클래스는 공부할 게 많다.");
-		msgObj.show();
+		TextMessage[] arr = new TextMessage[2];
+		showTextMessageAll(arr);
 		
-		msgObj = new TextMessageVer2("YG", "반복만이 살길이다.");
-		msgObj.show();
+//		TextMessage msgObj;
+//		msgObj = new TextMessage("클래스는 공부할 게 많다.");
+//		msgObj.show();
+//		
+//		msgObj = new TextMessageVer2("YG", "반복만이 살길이다.");
+//		msgObj.show();
 //		for(int i = 0; i < 2; i++) {
 //			if(i == 0) {
 //				msgObj = new TextMessage("클래스는 공부할 게 많다.");
